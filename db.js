cec5722e30b76460868def959fc291f196060d29
Abderrezak-WEBDEV <razik.bouiche@gmail.com>
2023-01-19 15:00:25 +0100

    initialisation de mongodb

---
+++ db.js
@@ -0,0 +1,1 @@
+// En premeir il faut installer  monfodb sur votre projet avec la commande 'npm i mongoose'
--- server.js
+++ server.js
@@ -1,1 +1,8 @@
 const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log('Node server Started using nodemon'));
+// tapez la commande sur le terminal 'node server'
+//puis la commande 'npm i nodemon'
+//puis la commande 'nodemon server'
